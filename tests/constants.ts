35a2b215181f6e3abf797c872e96eefa3a4a29da
Chii Yuen <chii.yuen@hotmail.com>
2026-01-15 23:57:53 +0800

    Fix incorrect feed ID

--- tests/constants.ts
+++ tests/constants.ts
@@ -10,7 +10,7 @@ export const SURFPOOL_RPC_URL = "http://127.0.0.1:8899";
 
 export const MINT_DECIMALS = 6;
 export const SOL_USD_FEED_ID =
-  "4cd1cad962425681af07b9254b7d804de3ca3446fbfd1371bb258d2c75059812";
+  "822512ee9add93518eca1c105a38422841a76c590db079eebb283deb2c14caa9";
 export const SOL_USD_ORACLE_QUOTE = OracleQuote.getCanonicalPubkey(
   ON_DEMAND_MAINNET_QUEUE,
   [SOL_USD_FEED_ID],
